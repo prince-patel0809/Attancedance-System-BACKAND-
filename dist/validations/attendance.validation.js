d910e492f5dcd1bff9764dc807247463b196e459
prince-patel0809 <propatel0809@gmail.com>
2026-03-26 08:22:12 +0530

    update

--- dist/validations/attendance.validation.js
+++ dist/validations/attendance.validation.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.markAttendanceSchema = void 0;
 const zod_1 = require("zod");
 exports.markAttendanceSchema = zod_1.z.object({
-    lecture_id: zod_1.z.string().uuid(),
+    lecture_id: zod_1.z.string(),
     latitude: zod_1.z.number(),
     longitude: zod_1.z.number()
 });
--- src/validations/attendance.validation.ts
+++ src/validations/attendance.validation.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const markAttendanceSchema = z.object({
-    lecture_id: z.string().uuid(),
+    lecture_id: z.string(),
     latitude: z.number(),
     longitude: z.number()
 });
